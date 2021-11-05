a3171719c15997a5a695e896a1e9973b59541ab5
Julien Martin <julienmartinlevrai@protonmail.ch>
2021-11-04 21:55:27 -0400

    test: add clipper

--- src/test/CurveCallee.t.sol
+++ src/test/CurveCallee.t.sol
@@ -57,12 +57,28 @@ interface Dog {
     function bark(bytes32, address, address) external returns (uint256);
 }
 
+interface Clipper {
+    function take(
+        uint256 id,
+        uint256 amt,
+        uint256 max,
+        address who,
+        bytes calldata data
+    ) external;
+}
+
 contract CurveCalleeTest is DSTest {
 
     address constant hevm     = 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
     address constant wstEth   = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
     address constant chainlog = 0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F;
     uint256 constant amt      = 50e18;
+    address constant curve    = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
+    address constant uniV3    = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    uint256 id;
+    address clipper;
+    CurveCallee callee;
     
     function setUp() public {
         // wstEth._balances[address(this)] = amt;
@@ -95,25 +111,21 @@ contract CurveCalleeTest is DSTest {
         address jug = Chainlog(chainlog).getAddress("MCD_JUG");
         Jug(jug).drip("WSTETH-A");
         address dog = Chainlog(chainlog).getAddress("MCD_DOG");
-        uint256 id = Dog(dog).bark("WSTETH-A", address(this), address(this));
+        id = Dog(dog).bark("WSTETH-A", address(this), address(this));
+        clipper = Chainlog(chainlog).getAddress("MCD_CLIP_WSTETH_A");
+        address daiJoin = Chainlog(chainlog).getAddress("MCD_JOIN_DAI");
+        callee = new CurveCallee(curve, uniV3, daiJoin);
     }
 
     function test() public {
-        log_uint(TokenLike(wstEth).balanceOf(address(this)));
-        address vat = Chainlog(chainlog).getAddress("MCD_VAT");
-        log_uint(VatLike(vat).gem("WSTETH-A", address(this)));
-        (uint256 ink, uint256 art) = VatLike(vat).urns("WSTETH-A", address(this));
-        log_uint(ink);
-        log_uint(art);
+        bytes memory data = abi.encode("");
+        Clipper(clipper).take({
+            id:   id,
+            amt:  amt,
+            max:  type(uint256).max,
+            who:  address(callee),
+            data: data
+        });
     }
 
-}
-
-interface TokenLike {
-    function balanceOf(address) external returns (uint256);
-}
-
-interface VatLike {
-    function gem(bytes32, address) external returns (uint256);
-    function urns(bytes32, address) external returns (uint256, uint256);
 }
