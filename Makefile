74d51314b1380335c09eaaf96272400f9aa6cae6
Christopher Mooney <chris@dod.net>
2021-11-14 12:04:41 -0500

    godsflaw-20211115: improves deploy scripts

--- Makefile
+++ Makefile
@@ -2,9 +2,12 @@ all             :; DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 dapp --use
 clean           :; dapp clean
 # usage:        make test match=Burn
 test            :; ./test.sh ${match}
-deploy          :; echo "use deploy-kovan or deploy-mainnet"
-deploy-kovan    :; make && ./scripts/deploy-kovan.sh
+deploy          :; echo "use deploy-goerli or deploy-mainnet"
+deploy-goerli   :; make && ./scripts/deploy-goerli.sh
 deploy-mainnet  :; make && ./scripts/deploy-mainnet.sh
 flatten         :;
 	hevm flatten --source-file "src/UniswapV2Callee.sol" > out/UniswapV2CalleeDai.sol
-	hevm flatten --source-file "src/UniswapV2LpTokenCallee.sol" > out/UniswapV2LpTokenCallee.sol
+	hevm flatten --source-file "src/UniswapV2LpTokenCallee.sol" > out/UniswapV2LpTokenCalleeDai.sol
+	hevm flatten --source-file "src/UniswapV3Callee.sol" > out/UniswapV3Callee.sol
+	hevm flatten --source-file "src/UniV3Callee.sol" > out/UniV3Callee.sol
+	hevm flatten --source-file "src/WstETHCurveUniv3Callee.sol" > out/WstETHCurveUniv3Callee.sol
---
+++ scripts/deploy-goerli.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+UniswapV2CalleeDai=$(dapp create UniswapV2CalleeDai \
+  0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D \
+  0x6a60b7070befb2bfc964F646efDF70388320f4E0)
+
+UniswapV2LpTokenCalleeDai=$(dapp create UniswapV2LpTokenCalleeDai \
+  0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D \
+  0x6a60b7070befb2bfc964F646efDF70388320f4E0)
+
+UniswapV3Callee=$(dapp create UniswapV3Callee \
+  0xE592427A0AEce92De3Edee1F18E0157C05861564 \
+  0x6a60b7070befb2bfc964F646efDF70388320f4E0)
+
+#
+# No curve pool for stETH on goerli yet
+#
+# UniV3Callee=$(dapp create UniV3Callee \
+#   0xE592427A0AEce92De3Edee1F18E0157C05861564 \
+#   0x6a60b7070befb2bfc964F646efDF70388320f4E0)
+#
+# WstETHCurveUniv3Callee=$(dapp create WstETHCurveUniv3Callee \
+#   0x0 \
+#   ${UniV3Callee} \
+#   0x6a60b7070befb2bfc964F646efDF70388320f4E0 \
+#   0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6)
+
+echo "UniswapV2CalleeDai: ${UniswapV2CalleeDai}"
+echo "UniswapV2LpTokenCalleeDai: ${UniswapV2LpTokenCalleeDai}"
+echo "UniswapV3Callee: ${UniswapV3Callee}"
+# echo "UniV3Callee: ${UniV3Callee}"
+# echo "WstETHCurveUniv3Callee: ${WstETHCurveUniv3Callee}"
