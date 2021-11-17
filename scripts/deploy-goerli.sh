32193a04ce4d9189039f0f721ad2194c8b7e962c
Christopher Mooney <chris@dod.net>
2021-11-17 13:32:52 -0500

    godsflaw-20211115: improves deploy scripts (#19)
    * godsflaw-20211115: improves deploy scripts
    
    * fixed addresses
    
    * fix: delete UniV3Callee
    
    * fix: remove UniV3Callee from déploy script
    
    * fix: remove UniV3Callee from Makefile
    
    * Update scripts/deploy-mainnet.sh
    
    Co-authored-by: Julien <80432849+julienmartinlevrai@users.noreply.github.com>
    
    * Update scripts/deploy-goerli.sh
    
    Co-authored-by: Julien <80432849+julienmartinlevrai@users.noreply.github.com>
    
    * spoke with curve, they are not on goerli
    
    Co-authored-by: Julien Martin <julienmartinlevrai@protonmail.ch>
    Co-authored-by: Julien <80432849+julienmartinlevrai@users.noreply.github.com>

--- scripts/deploy-goerli.sh
+++ scripts/deploy-goerli.sh
@@ -2,11 +2,16 @@
 
 UniswapV2CalleeDai=$(dapp create UniswapV2CalleeDai \
   0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D \
-  0x5AA71a3ae1C0bd6ac27A1f28e1415fFFB6F15B8c)
+  0x6a60b7070befb2bfc964F646efDF70388320f4E0)
 
 UniswapV2LpTokenCalleeDai=$(dapp create UniswapV2LpTokenCalleeDai \
   0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D \
-  0x5AA71a3ae1C0bd6ac27A1f28e1415fFFB6F15B8c)
+  0x6a60b7070befb2bfc964F646efDF70388320f4E0)
+
+UniswapV3Callee=$(dapp create UniswapV3Callee \
+  0xE592427A0AEce92De3Edee1F18E0157C05861564 \
+  0x6a60b7070befb2bfc964F646efDF70388320f4E0)
 
 echo "UniswapV2CalleeDai: ${UniswapV2CalleeDai}"
 echo "UniswapV2LpTokenCalleeDai: ${UniswapV2LpTokenCalleeDai}"
+echo "UniswapV3Callee: ${UniswapV3Callee}"
--- scripts/deploy-mainnet.sh
+++ scripts/deploy-mainnet.sh
@@ -8,5 +8,17 @@ UniswapV2LpTokenCalleeDai=$(dapp create UniswapV2LpTokenCalleeDai \
   0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D \
   0x9759A6Ac90977b93B58547b4A71c78317f391A28)
 
+UniswapV3Callee=$(dapp create UniswapV3Callee \
+  0xE592427A0AEce92De3Edee1F18E0157C05861564 \
+  0x9759A6Ac90977b93B58547b4A71c78317f391A28)
+
+WstETHCurveUniv3Callee=$(dapp create WstETHCurveUniv3Callee \
+  0xDC24316b9AE028F1497c275EB9192a3Ea0f67022 \
+  0xE592427A0AEce92De3Edee1F18E0157C05861564 \
+  0x9759A6Ac90977b93B58547b4A71c78317f391A28 \
+  0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
+
 echo "UniswapV2CalleeDai: ${UniswapV2CalleeDai}"
 echo "UniswapV2LpTokenCalleeDai: ${UniswapV2LpTokenCalleeDai}"
+echo "UniswapV3Callee: ${UniswapV3Callee}"
+echo "WstETHCurveUniv3Callee: ${WstETHCurveUniv3Callee}"
