9f65c61fdcad426cdbe05b8b874ca44996356fd3
Christopher Mooney <chris@dod.net>
2021-11-17 11:08:57 -0500

    spoke with curve, they are not on goerli

--- scripts/deploy-goerli.sh
+++ scripts/deploy-goerli.sh
@@ -12,16 +12,6 @@ UniswapV3Callee=$(dapp create UniswapV3Callee \
   0xE592427A0AEce92De3Edee1F18E0157C05861564 \
   0x6a60b7070befb2bfc964F646efDF70388320f4E0)
 
-#
-# No curve pool for stETH on goerli yet
-#
-# WstETHCurveUniv3Callee=$(dapp create WstETHCurveUniv3Callee \
-#   0x0 \
-#   0xE592427A0AEce92De3Edee1F18E0157C05861564 \
-#   0x6a60b7070befb2bfc964F646efDF70388320f4E0 \
-#   0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6)
-
 echo "UniswapV2CalleeDai: ${UniswapV2CalleeDai}"
 echo "UniswapV2LpTokenCalleeDai: ${UniswapV2LpTokenCalleeDai}"
 echo "UniswapV3Callee: ${UniswapV3Callee}"
-# echo "WstETHCurveUniv3Callee: ${WstETHCurveUniv3Callee}"
