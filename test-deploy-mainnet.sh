9519f7be9d9365c7e1ca584c4b41a648c84baa73
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2021-11-19 16:04:36 -0700

    focus on simulation tests

--- test-deploy-mainnet.sh
+++ test-deploy-mainnet.sh
@@ -22,16 +22,13 @@ export UniswapV2LpTokenCalleeDai=$(echo $deploy_output| cut -d' ' -f 2 | cut -d'
 export UniswapV3Callee=$(echo $deploy_output| cut -d' ' -f 3 | cut -d'=' -f 2)
 export WstETHCurveUniv3Callee=$(echo $deploy_output| cut -d' ' -f 4 | cut -d'=' -f 2)
 
-for file in ./src/test/*.sol
-do
-    new_file="./src/test/deploy-$(echo $file | cut -d'/' -f 4)"
-    cat $file                                                                                                                    \
-      | sed -E "s|new UniswapV2CalleeDai\([a-zA-Z0-9(), ]*\)|UniswapV2CalleeDai\($UniswapV2CalleeDai\)|g"                        \
-      | sed -E "s|new UniswapV2LpTokenCalleeDai\([a-zA-Z0-9(), ]*\)|UniswapV2LpTokenCalleeDai\($UniswapV2LpTokenCalleeDai\)|g"   \
-      | sed -E "s|new UniswapV3Callee\([a-zA-Z0-9(), ]*\)|UniswapV3Callee\($UniswapV3Callee\)|g"                                 \
-      | sed -E "s|new WstETHCurveUniv3Callee\([a-zA-Z0-9(), ]*\)|WstETHCurveUniv3Callee\($WstETHCurveUniv3Callee\)|g"            \
-      >> "$new_file"
-done
+new_file="./src/test/deploy-Simulation.t.sol"
+cat ./src/test/Simulation.t.sol                                                                                                                    \
+    | sed -E "s|new UniswapV2CalleeDai\([a-zA-Z0-9(), ]*\)|UniswapV2CalleeDai\($UniswapV2CalleeDai\)|g"                        \
+    | sed -E "s|new UniswapV2LpTokenCalleeDai\([a-zA-Z0-9(), ]*\)|UniswapV2LpTokenCalleeDai\($UniswapV2LpTokenCalleeDai\)|g"   \
+    | sed -E "s|new UniswapV3Callee\([a-zA-Z0-9(), ]*\)|UniswapV3Callee\($UniswapV3Callee\)|g"                                 \
+    | sed -E "s|new WstETHCurveUniv3Callee\([a-zA-Z0-9(), ]*\)|WstETHCurveUniv3Callee\($WstETHCurveUniv3Callee\)|g"            \
+    >> "$new_file"
 
 if [[ $# -eq 0 ]] ; then
     dapp --use solc:0.6.12 test --rpc --verbosity 3 -m "deploy-"
@@ -39,7 +36,4 @@ else
     dapp --use solc:0.6.12 test --rpc --verbosity 3 -m ${1}
 fi
 
-for file in ./src/test/deploy-*.sol
-do
-    rm $file
-done
+# rm ./src/test/deploy-Simulation.t.sol
