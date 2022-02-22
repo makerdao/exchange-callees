867d071282fd63709dec4fa05a45f82435405d0c
Tal Baneth <talbaneth@gmail.com>
2022-02-22 03:59:19 +0200

    Use proposed offboarding configuration

--- src/test/TUSDCurveCallee.t.sol
+++ src/test/TUSDCurveCallee.t.sol
@@ -18,6 +18,7 @@ pragma solidity ^0.6.12;
 
 import "ds-test/test.sol";
 import { TUSDCurveCallee } from "../TUSDCurveCallee.sol";
+import { LinearDecrease } from "dss/abaci.sol";
 
 interface Hevm {
     function store(address c, bytes32 loc, bytes32 val) external;
@@ -55,13 +56,14 @@ interface Vat {
     function hope(address) external;
 }
 
-interface Jug {
-    function drip(bytes32) external;
-}
-
 interface Dog {
     function bark(bytes32, address, address) external returns (uint256);
     function file(bytes32, bytes32, uint256) external;
+}
+
+interface Spot {
+    function file(bytes32, bytes32, uint256) external;
+    function poke(bytes32) external;
 }
 
 interface Clipper {
@@ -74,6 +76,7 @@ interface Clipper {
         bytes calldata data
     ) external;
     function file(bytes32 what, uint256 data) external;
+    function file(bytes32 what, address data) external;
 }
 
 interface Osm {
@@ -88,6 +91,8 @@ contract CurveCalleeTest is DSTest {
     address constant chainlog = 0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F;
     address constant curve    = 0xEcd5e75AFb02eFa118AF914515D6521aaBd189F1;
     uint256 constant WAD      = 1e18;
+    uint256 constant RAY      = 1e27;
+    uint256 constant RAD      = 1e45;
 
     address gemJoin;
     uint256 id;
@@ -95,6 +100,9 @@ contract CurveCalleeTest is DSTest {
     TUSDCurveCallee callee;
     uint256 tail;
     address vat;
+    address dog;
+    address dai;
+    LinearDecrease calc;
 
     function giveTokens(address token, uint256 amount) internal {
         // Edge case - balance is already set for some reason
@@ -149,31 +157,63 @@ contract CurveCalleeTest is DSTest {
         address daiJoin = Chainlog(chainlog).getAddress("MCD_JOIN_DAI");
         callee = new TUSDCurveCallee(curve, daiJoin);
         vat = Chainlog(chainlog).getAddress("MCD_VAT");
+        dog = Chainlog(chainlog).getAddress("MCD_DOG");
+        dai = Chainlog(chainlog).getAddress("MCD_DAI");
+        gemJoin = Chainlog(chainlog).getAddress("MCD_JOIN_TUSD_A");
+        calc = new LinearDecrease();
+
         Vat(vat).hope(clipper);
+
+        takeOwnership(dog);
+        takeOwnership(clipper);
+
+        // Enable liquidations
+        Clipper(clipper).file("stopped", 0);
+
+        // Use Abacus/LinearDecrease – tau [seconds]
+        Clipper(clipper).file("calc", address(calc));
+
+        // Set liquidation penalty (chop) to 0
+        Dog(dog).file("TUSD-A", "chop", 1.0 * WAD);
+
+        // Set LR to 150%
+        // This is simulated in newAuction() after opening a new auction
+
+        // Set buff to 1
+        Clipper(clipper).file("buf", 1.0 * RAY); // 0% Initial price buffer
+
+        // Set tau to 432,000,000 second (est. 10bps drop per 5 days = 5000 days till 0)
+        calc.file(bytes32("tau"), 432_000_000);
+
+        // Cusp not relevant, stays the same (tail will stop the auction)
+        // (Currently 90% decrease)
+
+        // Set tail to 2,160,000 (25 days) (implies minimum price of 0.995)
+        Clipper(clipper).file("tail", 2_160_000);
         tail = Clipper(clipper).tail();
 
-        // Turn on liquidations:
+        // Set hole on 30m (ideally we want all vaults kicked at once so that same auction curve applies)
+        Dog(dog).file("TUSD-A", "hole", 30_000_000 * RAD);
 
-        address dog = Chainlog(chainlog).getAddress("MCD_DOG");
-        takeOwnership(dog);
-        Dog(dog).file("TUSD-A", "hole", type(uint256).max);
-        renounceOwnership(dog);
+        // Set chip to 0 (no need to be fast)
+        Clipper(clipper).file("chip", 0);
 
-        takeOwnership(clipper);
-        Clipper(clipper).file("stopped", 0);
+        // Set tip to 500 (kick all at start to achieve linear price discount, a bit higher value so that we guarantee kicking them all - ideally PE helps here)
+        Clipper(clipper).file("tip", 500 * RAD);
+
         renounceOwnership(clipper);
+        renounceOwnership(dog);
     }
 
     function newAuction(uint256 amt) internal {
-        // tusd._balances[address(this)] = amt;
         giveTokens(tusd, amt);
 
-        gemJoin = Chainlog(chainlog).getAddress("MCD_JOIN_TUSD_A");
         Token(tusd).approve(gemJoin, amt);
         Join(gemJoin).join(address(this), amt);
         (, uint256 rate, uint256 spot,,) = Vat(vat).ilks("TUSD-A");
         uint256 maxArt = amt * spot / rate;
 
+        // Support creating more positions (for testing only)
         takeOwnership(vat);
         Vat(vat).file("TUSD-A", "line", type(uint256).max);
         renounceOwnership(vat);
@@ -187,11 +227,13 @@ contract CurveCalleeTest is DSTest {
             dart: int(maxArt)
         });
 
-        takeOwnership(vat);
-        Vat(vat).file("TUSD-A", "spot", spot * 99 / 100); // Simulate reducing liquidation ratio
-        renounceOwnership(vat);
+        // Set LR to 150%
+        address spotter = Chainlog(chainlog).getAddress("MCD_SPOT");
+        takeOwnership(spotter);
+        Spot(spotter).file("TUSD-A", bytes32("mat"), 150 * RAY / 100);
+        renounceOwnership(spotter);
+        Spot(spotter).poke("TUSD-A");
 
-        address dog = Chainlog(chainlog).getAddress("MCD_DOG");
         id = Dog(dog).bark("TUSD-A", address(this), address(this));
     }
 
@@ -212,7 +254,6 @@ contract CurveCalleeTest is DSTest {
             data: data
         });
 
-        address dai = Chainlog(chainlog).getAddress("MCD_DAI");
         assertEq(Token(dai).balanceOf(address(this)), 0);
         assertEq(Token(tusd).balanceOf(address(this)), 0);
     }
@@ -234,20 +275,19 @@ contract CurveCalleeTest is DSTest {
             who:  address(callee),
             data: data
         });
-        address dai = Chainlog(chainlog).getAddress("MCD_DAI");
         assertGe(Token(dai).balanceOf(address(123)), minProfit);
     }
 
     function test_profitBigAmt() public {
         uint256 minProfit = 1000 * WAD;
-        uint256 amt = 10_000_000 * WAD;
+        uint256 amt = 6_000_000 * WAD;
         newAuction(amt);
         bytes memory data = abi.encode(
             address(123),
             address(gemJoin),
             uint256(minProfit)
         );
-        Hevm(hevm).warp(block.timestamp + tail / 2);
+        Hevm(hevm).warp(block.timestamp + tail / 3);
         Clipper(clipper).take({
             id:   id,
             amt:  amt,
@@ -255,7 +295,6 @@ contract CurveCalleeTest is DSTest {
             who:  address(callee),
             data: data
         });
-        address dai = Chainlog(chainlog).getAddress("MCD_DAI");
         assertGe(Token(dai).balanceOf(address(123)), minProfit);
     }
 
@@ -268,7 +307,9 @@ contract CurveCalleeTest is DSTest {
             address(gemJoin),
             uint256(minProfit)
         );
-        Hevm(hevm).warp(block.timestamp + tail / 2);
+
+        // Jump ahead to when price goes down enough to make up for the slippage
+        Hevm(hevm).warp(block.timestamp + tail * 9000 / 10000);
         Clipper(clipper).take({
             id:   id,
             amt:  amt,
@@ -276,7 +317,6 @@ contract CurveCalleeTest is DSTest {
             who:  address(callee),
             data: data
         });
-        address dai = Chainlog(chainlog).getAddress("MCD_DAI");
         assertGe(Token(dai).balanceOf(address(123)), minProfit);
     }
 
@@ -288,31 +328,8 @@ contract CurveCalleeTest is DSTest {
             address(gemJoin),
             uint256(0)
         );
-        Hevm(hevm).warp(block.timestamp + tail / 2);
-        address osm = Chainlog(chainlog).getAddress("PIP_TUSD");
-        Hevm(hevm).store({
-            c:   osm,
-            loc: keccak256(abi.encode(address(this), uint256(0))),
-            val: bytes32(uint256(1))
-        });
-        uint256 max = uint256(Osm(osm).read()) * 1e9; // WAD * 1e9 = RAY
-        Clipper(clipper).take({
-            id:   id,
-            amt:  amt,
-            max:  max,
-            who:  address(callee),
-            data: data
-        });
-    }
 
-    function testFail_maxPrice() public {
-        uint256 amt = 20000 * WAD;
-        newAuction(amt);
-        bytes memory data = abi.encode(
-            address(this),
-            address(gemJoin),
-            uint256(0)
-        );
+        // Make sure price goes down enough to make up for the Curve slippage
         Hevm(hevm).warp(block.timestamp + tail / 5);
         address osm = Chainlog(chainlog).getAddress("PIP_TUSD");
         Hevm(hevm).store({
